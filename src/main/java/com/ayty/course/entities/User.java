8b2860e34509940eb93a078f8c900d363cf8b47d
lucaassp <lucaspereira.student@gmail.com>
2022-05-23 13:30:39 -0300

    H2 database, test profile, JPA

--- src/main/java/com/ayty/course/entities/User.java
+++ src/main/java/com/ayty/course/entities/User.java
@@ -2,15 +2,22 @@ package com.ayty.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
-
-    private Long id;
-    private String name;
-    private String email;
-    private String phone;
-    private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
     
     public User() {
     }
