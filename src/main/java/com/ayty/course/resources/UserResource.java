716db3430862f9342356cdb7582742c67b9da956
lucaassp <lucaspereira.student@gmail.com>
2022-06-27 23:11:55 -0300

    User insert

--- src/main/java/com/ayty/course/resources/UserResource.java
+++ src/main/java/com/ayty/course/resources/UserResource.java
@@ -1,6 +1,9 @@
 package com.ayty.course.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.Servlet;
 
 import com.ayty.course.entities.User;
 import com.ayty.course.services.UserService;
@@ -9,8 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController // Web resource that is implemented by a Rest controller
 @RequestMapping(value = "/users") // Resource name
@@ -32,4 +38,10 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
