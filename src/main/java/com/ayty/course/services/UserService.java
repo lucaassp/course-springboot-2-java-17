a4e6c72f6130cc9df6c582e2b9ce2464f49fdd6e
lucaassp <lucaspereira.student@gmail.com>
2022-07-02 21:10:07 -0300

    Exception handling - delete

--- src/main/java/com/ayty/course/services/UserService.java
+++ src/main/java/com/ayty/course/services/UserService.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 
 import com.ayty.course.entities.User;
 import com.ayty.course.repositories.UserRepository;
+import com.ayty.course.services.exceptions.DatabaseException;
 import com.ayty.course.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+        
     }
 
     public User update(Long id, User obj) {
